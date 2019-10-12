abab530940bc52c7f9f82333a134f8f76aa0aac9
Kristin <1358365@DESKTOP-4FH9G41>
2019-10-12 21:35:53 +0300

    Prototype of authorisation without DB added

--- src/main/java/service/controller/Controller.java
+++ src/main/java/service/controller/Controller.java
@@ -1,14 +1,28 @@
 package service.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("/greeting")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private String name = "";
+    private String password = "";
+
+    @RequestMapping(path="/join", method = RequestMethod.GET)
+    public @ResponseBody String addNewUser (@RequestParam String name,
+                                            @RequestParam String password) {
+        this.name = name;
+        this.password = password;
+
+        return "User with name " + name + " with password " + password + " was successfully saved" ;
     }
 
+    @RequestMapping(path="/login", method = RequestMethod.GET)
+    public @ResponseBody String loginIntoSystem(@RequestParam String name,
+                                                @RequestParam String password) {
+        if (this.name.equals(name) && this.password.equals(password))
+            return "authorisation was successful";
+        else
+            return "authorisation failed";
+    }
 }
