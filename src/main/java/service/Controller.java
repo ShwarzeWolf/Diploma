48e66ea22bff4c24073f120b9f2709f4b7720d86
Ivan <i_am_vanya@mail.ru>
2019-10-14 16:07:29 +0300

    adeed UserForm

--- src/main/java/service/Controller.java
+++ src/main/java/service/Controller.java
@@ -1,14 +1,25 @@
 package service;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Controller {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
     }
+//    public String index() {
+//        return "Greetings from Spring Boot!";
+//    }
 
 }
