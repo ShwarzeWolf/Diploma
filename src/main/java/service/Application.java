ac35bfd790c5b4274bbaacd13d719358d355b375
Ivan <i_am_vanya@mail.ru>
2019-10-19 20:27:37 +0300

    connecting JSP in project

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -1,18 +1,37 @@
 package service;
+//
+//// import java.util.Arrays;
+//// import service.controller.Controller;
+//// import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//// import org.springframework.context.ApplicationContext;
+//// import org.springframework.context.annotation.Bean;
+//
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//}
 
-// import java.util.Arrays;
-// import service.controller.Controller;
-// import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.ApplicationContext;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/service/controller/Controller1.java
@@ -0,0 +1,56 @@
+package service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Controller1 {
+//    private UsersManager users;
+
+//    public Controller() {
+//        super();
+//        try {
+//            users = new UsersManager();
+//        } catch (BLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @RequestMapping(path="/register", method = RequestMethod.GET)
+//    public @ResponseBody String addNewUser (@RequestParam String email,
+//                                            @RequestParam String name,
+//                                            @RequestParam String password) {
+//        try {
+//            users.addUser(name, email, password);
+//            return "User with name " + name + " with password " + password + " was successfully saved" ;
+//        } catch (BLException ex) {
+//            // ex.printStackTrace();
+//            throw new RuntimeException(ex);
+//            // return "User addition failed";
+//        }
+//    }
+//
+//    @RequestMapping(path="/login", method = RequestMethod.GET)
+//    public @ResponseBody String loginIntoSystem(@RequestParam String email,
+//                                                @RequestParam String password) {
+//        if (users.passwordOkay(email, password))
+//            return "authorisation was successful";
+//        else
+//            return "authorisation failed";
+//    }
+
+//    @RequestMapping(path="/", method = RequestMethod.GET)
+//    public @ResponseBody String loginIntoSystem() {
+//        return "<a href=\"/refister\">Register</a><br><a href=\"/login\">Login</a>";
+//    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello() {
+//        model.addAttribute("message", "Hello Spring MVC Framework!");
+
+        return "hello";
+    }
+}
