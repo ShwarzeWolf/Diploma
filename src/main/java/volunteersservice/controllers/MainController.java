cab1daddbc454e0d5bfc008cf0f99ba35ecbf232
Kristin <1358365@DESKTOP-4FH9G41>
2019-11-22 03:34:08 +0300

    commit before pulling info

--- src/main/java/volunteersservice/controllers/MainController.java
+++
@@ -1,9 +0,0 @@
-package volunteersservice.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MainController {
-}
